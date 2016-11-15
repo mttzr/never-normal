b1fc8b6a28b1e140aba706e011c687a66eae561b
Matthew Zehnder <zehnder.matt@gmail.com>
2016-11-14 23:31:32 -0800

    first never normal commit

---
+++ content.js
@@ -0,0 +1,3 @@
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Trump", "ig"), "Trump (the sexual predator / racist)");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Pence", "ig"), "Pence (the homophobe)");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("bannon", "ig"), "Bannon (the white supremacist)");
